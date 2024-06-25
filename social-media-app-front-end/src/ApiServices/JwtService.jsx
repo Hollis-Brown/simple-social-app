239b0611e7f30ec908e0ce8e6b31a3c5f17b8889
Hollis-Brown <123035058+Hollis-Brown@users.noreply.github.com>
2024-06-24 17:38:36 -0700

    Update JwtService.jsx

--- social-media-app-front-end/src/ApiServices/JwtService.jsx
+++ social-media-app-front-end/src/ApiServices/JwtService.jsx
@@ -1,20 +1,16 @@
 const jwtKey = 'social-media-app-jwt';
 
 
-// setJwt: Saves your secure login token (JWT) for the app.
 export const setJwt = (jwt) => {
   localStorage.setItem(jwtKey, jwt);
 }
 
-// getJwt: Grabs your secure login token (JWT) from the app storage.
 export const getJwt = () => {
   const jwt = localStorage.getItem(jwtKey);
   return jwt;
 }
 
-// removeJwt: Deletes your secure login token (JWT) when you log out.
 export const removeJwt = () => {
   localStorage.removeItem(jwtKey);
 }
 
-// It stores, retrieves, and removes JWTs from local storage using a secret key.
