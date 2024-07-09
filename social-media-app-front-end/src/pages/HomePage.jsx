1e94bef4c9833cbb68af33bd01594cca53624c21
Hollis-Brown <hollis.brown.cohort231@gmail.com>
2024-07-08 19:27:59 -0700

    Remove redundant and/or unwanted files

--- social-media-app-front-end/src/pages/HomePage.jsx
+++ social-media-app-front-end/src/pages/HomePage.jsx
@@ -9,20 +9,24 @@ function Home() {
   return (
     <div className="flex">
       <Navbar />
-      <div className="w-64">
+      <div className="flex flex-1 overflow-hidden pt-16">
+      <div className="hidden md:block w-64">
         <Sidebar />
       </div>
       <div className="flex-grow mt-4 max-w-4xl mx-auto gap-6 overflow-auto scrollbar-hide">
-        <div className="flex flex-col mt-14">
+        <div className="flex flex-col">
           <h1 className="text-6xl mb-4 text-gray-300">Posts</h1>
           <PostForm className="lg:mr-20"/>
           <Card>
             <Posts />
           </Card>
         </div>
-      </div>
+        </div>
+        </div>
     </div>
   );
 }
 
 export default Home;
+
+
